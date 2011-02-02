3521fdf2a64e224c642769484938641821f3a862
Joe Mahoney <joe@3months.com>
2011-02-03 08:38:29 +1300

    restored auth token

--- app/controllers/ckeditor/base_controller.rb
+++ app/controllers/ckeditor/base_controller.rb
@@ -1,5 +1,5 @@
 class Ckeditor::BaseController < ApplicationController
-  skip_before_filter :verify_authenticity_token
+  skip_before_filter :verify_authenticity_token, :only => [:create]
   
   before_filter :swf_options, :only => [:index, :create]
   before_filter :find_asset, :only => [:destroy]
